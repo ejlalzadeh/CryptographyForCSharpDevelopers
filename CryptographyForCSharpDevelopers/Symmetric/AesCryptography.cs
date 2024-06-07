889e4fa913deae31cc115ea101b0da3d7bb7fce9
Ali Ejlalzadeh <aliejlalzadeh1379@gmail.com>
2024-06-07 11:41:19 +0330

    Add AES Cryptography

--- CryptographyForCSharpDevelopers/Symmetric/AesCryptography.cs
+++ CryptographyForCSharpDevelopers/Symmetric/AesCryptography.cs
@@ -1,6 +1,60 @@
-﻿namespace CryptographyForCSharpDevelopers.Symmetric;
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptographyForCSharpDevelopers.Symmetric;
 
 public class AesCryptography
 {
+    public static string Decrypt(string cipherText, string primaryKey, string? secondaryKey = null)
+    {
+        byte[] iv = new byte[16];
 
+        if (!string.IsNullOrWhiteSpace(secondaryKey))
+            iv = Encoding.ASCII.GetBytes(secondaryKey);
+
+        byte[] buffer = Convert.FromBase64String(cipherText);
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(primaryKey); aes.IV = iv;
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            using (MemoryStream memoryStream = new MemoryStream(buffer))
+            {
+                using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                    {
+                        return streamReader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+
+    public static string Encrypt(string plainText, string primaryKey, string? secondaryKey = null)
+    {
+        byte[] iv = new byte[16];
+
+        if (!string.IsNullOrWhiteSpace(secondaryKey))
+            iv = Encoding.ASCII.GetBytes(secondaryKey);
+
+        byte[] array;
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(primaryKey);
+            aes.IV = iv;
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                    {
+                        streamWriter.Write(plainText);
+                    }
+                    array = memoryStream.ToArray();
+                }
+            }
+        }
+        return Convert.ToBase64String(array);
+    }
 }
